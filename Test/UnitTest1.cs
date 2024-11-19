4eafd39b2545dddb91594ecad51e12f381752459
Jafar Safdari <jafar.safdari@winlas.se>
2024-11-19 22:23:15 +0100

    feat: code coverage with Coverlet.

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,10 +1,19 @@
-namespace Test;
+namespace codecoveragetest;
 
 public class UnitTest1
 {
+    
     [Fact]
     public void Test1()
     {
-
+        var test1 = Class1.car("test");
+        Assert.Equal("test", test1);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var test1 = Class1.car("test3");
+        Assert.Equal("test3", test1);
     }
 }
